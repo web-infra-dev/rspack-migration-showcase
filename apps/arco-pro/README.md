3eebadbf8ffdd5007b0fc3f2edcb9b810957c4de
hardfist <yangjianzju@gmail.com>
2023-02-23 10:57:30 +0800

    chore: add arco

---
+++ apps/arco-pro/README.md
@@ -0,0 +1,16 @@
+# Arco Design Pro
+
+## 快速开始
+
+```
+// 初始化项目
+npm install
+
+// 开发模式
+npm run dev
+
+// 构建
+npm run build
+
+// 包分析
+npm run analyze
---
+++ apps/arco-pro/rspack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const htmlPlugin = require('@rspack/plugin-html').default;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const isProd = process.env.NODE_ENV === 'production';
+const mode = isProd ? 'production' : 'development';
+const ana = process.env.ANA === 'true';
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  mode,
+  context: __dirname,
+  entry: { main: './src/index.tsx' },
+  devServer: {
+    port: 5555,
+  },
+  plugins:
+    [
+      new htmlPlugin({
+        template: './index.html',
+        publicPath: '/',
+      }),
+    ],
+  module: {
+    rules:
+      [
+        {
+          test: /\.scss$/,
+          use:
+            [
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          type: 'css',
+        },
+        {
+          test: /\.module\.scss$/,
+          use:
+            [
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          type: 'css/module',
+        },
+        {
+          test: /\.less$/,
+          use: [{ loader: require.resolve('less-loader') }],
+          type: 'css'
+        },
+        {
+          test: /module\.less$/,
+          use: [{ loader: require.resolve('less-loader') }],
+          type: 'css/module',  // native css module
+        },
+        {
+          test: /\.svg$/,
+          use:
+            [
+              {
+                loader: '@svgr/webpack',
+              },
+            ],
+          type: 'js',
+        },
+      ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      bizchars: require.resolve('bizcharts/es/index.js')
+    }
+  },
+  output: {
+    publicPath: '/',
+  },
+  infrastructureLogging: {
+    debug: false,
+  },
+  stats: {
+    all: false,
+    errors: true,
+    warnings: true,
+  },
+};
